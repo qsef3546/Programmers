7f7171ad8fffd44e86870c33837ffd8d55c65db1
qsef354610@gmail.com <qsef354610@gmail.com>
2022-07-30 20:21:28 +0900

    code

--- Python/2021 카카오 채용연계형 인턴십/숫자 문자열과 영단어.py
+++ Python/2021 카카오 채용연계형 인턴십/숫자 문자열과 영단어.py
@@ -1,14 +1,18 @@
 example1 = "one4seveneight"
 
-
+#숫자 사전
 digit = {'zero':'0','one':'1','two':'2','three':'3','four':'4',
          'five':'5','six':'6','seven':'7','eight':'8','nine':'9'}
 
 
 def solution(s):
     res = s
+
+    #영문자는 숫자(string)으로 변경
     for i in digit.keys():
         res= res.replace(i,digit[i])
+        
+    #convert string to int
     answer = int(res)
     return answer
 
