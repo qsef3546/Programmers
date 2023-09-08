1f4d20fb678606c99ab7e143b2b74c08adc25c5e
qsef3546 <qsef354610@gmail.com>
2023-09-08 22:54:20 +0900

    푸드파이트대회 (주석 추가)

--- C++/Lv1/푸드파이트대회.cpp
+++ C++/Lv1/푸드파이트대회.cpp
@@ -5,22 +5,22 @@ using namespace std;
 
 string solution(vector<int> food) {
     string answer = "";
-    vector<int> front;
-    vector<int> back;
+    vector<int> front; // 선수1 음식 vector
+    vector<int> back;  // 선수2 음식 vector
     stringstream ss;
 
-    int count = 1;
+    int count = 1; // 칼로리
 
     for (int i = 1; i < food.size(); i++) {
-        front.insert(front.end(), food[i] / 2, count);
-        back.insert(back.begin(), food[i] / 2, count);
+        front.insert(front.end(), food[i] / 2, count); // 선수1은 칼로리가 적은 음식을 오름차순으로 추가
+        back.insert(back.begin(), food[i] / 2, count); // 선수2는 칼로리가 적은 음식을 내림차순으로 추가
         count++;
     }
-    front.emplace_back(0);
-    front.insert(front.end(), back.begin(), back.end());
-    for (auto i : front) ss << i;
+    front.emplace_back(0);  // 종료를 나타내는 (0) 추가
+    front.insert(front.end(), back.begin(), back.end()); //선수1과 선수2의 음식 순서 합치기
+    for (auto i : front) ss << i; // 합친 음식 순서를 문자열로 만들기 위하여 stream에 대입
 
-    answer = ss.str();
+    answer = ss.str(); // 음식 순서 문자열화
 
     return answer;
 }
